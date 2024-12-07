28733b42719af605274fa3f14bf85aee81c0214e
John Pignata <john@pignata.com>
2024-12-06 22:11:04 -0500

    Initial substack-posts spike

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,12 +3,12 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './whats-alf'
+import * as substackPosts from './substack-posts'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
+  [substackPosts.shortname]: substackPosts.handler,
 }
 
 export default algos
--- src/subscription.ts
+++ src/subscription.ts
@@ -2,35 +2,55 @@ import {
   OutputSchema as RepoEvent,
   isCommit,
 } from './lexicon/types/com/atproto/sync/subscribeRepos'
+import { ViewExternal } from './lexicon/types/app/bsky/embed/external'
 import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
+import { URL } from "url";
+
+interface Post {
+	uri: string;
+	cid: string;
+	indexedAt: string;
+}
 
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
   async handleEvent(evt: RepoEvent) {
     if (!isCommit(evt)) return
 
     const ops = await getOpsByType(evt)
+    const postsToDelete = ops.posts.deletes.map((del) => del.uri);
+    const postsToCreate: Post[] = []; 
 
-    // This logs the text of every post off the firehose.
-    // Just for fun :)
-    // Delete before actually using
-    for (const post of ops.posts.creates) {
-      console.log(post.record.text)
-    }
+		for (const post of ops.posts.creates) {
+			if (post.record.embed && post.record.embed.external) {
+				const embed = post.record.embed.external as ViewExternal;
+				const uri = new URL(embed.uri);
 
-    const postsToDelete = ops.posts.deletes.map((del) => del.uri)
-    const postsToCreate = ops.posts.creates
-      .filter((create) => {
-        // only alf-related posts
-        return create.record.text.toLowerCase().includes('alf')
-      })
-      .map((create) => {
-        // map alf-related posts to a db row
-        return {
-          uri: create.uri,
-          cid: create.cid,
-          indexedAt: new Date().toISOString(),
-        }
-      })
+				if (uri.pathname.includes('/p/')) {
+					if (uri.hostname.endsWith('substack.com')) {
+						postsToCreate.push({
+							uri: post.uri,
+							cid: post.cid,
+							indexedAt: new Date().toISOString(),
+						});
+					} else {
+						try {
+							const response = await fetch(uri, {method: "HEAD", redirect: "follow"});
+							const cluster = response.headers.get('x-cluster');
+
+							if (cluster === 'substack') {
+								postsToCreate.push({
+									uri: post.uri,
+									cid: post.cid,
+									indexedAt: new Date().toISOString(),
+								});
+							}
+						} catch {
+							// ignore
+						}
+					}
+				}
+			}
+		}
 
     if (postsToDelete.length > 0) {
       await this.db
