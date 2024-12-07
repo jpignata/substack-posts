f0a69a76bb867989d6c1a651a931895bd5cb3250
John Pignata <john@pignata.com>
2024-12-07 16:27:04 -0500

    Fix crazy indent
    - Ignore SQLite databases

--- src/subscription.ts
+++ src/subscription.ts
@@ -7,9 +7,9 @@ import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
 import { URL } from "url";
 
 interface Post {
-	uri: string;
-	cid: string;
-	indexedAt: string;
+  uri: string;
+  cid: string;
+  indexedAt: string;
 }
 
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
@@ -20,50 +20,50 @@ export class FirehoseSubscription extends FirehoseSubscriptionBase {
     const postsToDelete = ops.posts.deletes.map((del) => del.uri);
     const postsToCreate: Post[] = []; 
 
-		for (const post of ops.posts.creates) {
-			if (post.record.embed && post.record.embed.external) {
-				const embed = post.record.embed.external as ViewExternal;
-				const uri = new URL(embed.uri);
+    for (const post of ops.posts.creates) {
+      if (post.record.embed && post.record.embed.external) {
+        const embed = post.record.embed.external as ViewExternal;
+        const uri = new URL(embed.uri);
 
-				if (uri.pathname.includes('/p/')) {
-					if (uri.hostname.endsWith('substack.com')) {
-						postsToCreate.push({
-							uri: post.uri,
-							cid: post.cid,
-							indexedAt: new Date().toISOString(),
-						});
-					} else {
-						try {
-							const response = await fetch(uri, {method: "HEAD", redirect: "follow"});
-							const cluster = response.headers.get('x-cluster');
+        if (uri.pathname.includes('/p/')) {
+          if (uri.hostname.endsWith('substack.com')) {
+            postsToCreate.push({
+              uri: post.uri,
+              cid: post.cid,
+              indexedAt: new Date().toISOString(),
+            });
+          } else {
+            try {
+              const response = await fetch(uri, {method: "HEAD", redirect: "follow"});
+              const cluster = response.headers.get('x-cluster');
 
-							if (cluster === 'substack') {
-								postsToCreate.push({
-									uri: post.uri,
-									cid: post.cid,
-									indexedAt: new Date().toISOString(),
-								});
-							}
-						} catch {
-							// ignore
-						}
-					}
-				}
-			}
-		}
+              if (cluster === 'substack') {
+                postsToCreate.push({
+                  uri: post.uri,
+                  cid: post.cid,
+                  indexedAt: new Date().toISOString(),
+                });
+              }
+            } catch {
+              // ignore
+            }
+          }
+        }
+      }
+    }
 
     if (postsToDelete.length > 0) {
       await this.db
-        .deleteFrom('post')
-        .where('uri', 'in', postsToDelete)
-        .execute()
+      .deleteFrom('post')
+      .where('uri', 'in', postsToDelete)
+      .execute()
     }
     if (postsToCreate.length > 0) {
       await this.db
-        .insertInto('post')
-        .values(postsToCreate)
-        .onConflict((oc) => oc.doNothing())
-        .execute()
+      .insertInto('post')
+      .values(postsToCreate)
+      .onConflict((oc) => oc.doNothing())
+      .execute()
     }
   }
 }
